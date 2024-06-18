8fef088cbbda003273fd0fe75004ce2044bf0b19
Saurabh Sharma <tsss.1920@gmail.com>
2024-06-18 14:52:24 +0530

    Add project files.

---
+++ CapitalPlacementTask/Models/PersonalInformation.cs
@@ -0,0 +1,16 @@
+﻿namespace CapitalPlacementTask.Models
+{
+    public class PersonalInformation
+    {
+        public int Id { get; set; }
+        public string?  FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Nationality { get; set; }
+        public string? CurrentResidence { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Gender {  get; set; }
+
+    }
+}
---
+++ CapitalPlacementTask/Models/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace CapitalPlacementTask.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string QuestionName { get; set; }
+        public int MaxChoiceAllowed { get; set; }
+
+
+    }
+}
